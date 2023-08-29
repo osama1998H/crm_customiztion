69f27759a20673a01a79809fc21e4535deeae32e
Sourcery AI <>
2023-08-29 08:32:04 +0000

    'Refactored by Sourcery'

--- crm.py
+++ crm.py
@@ -5,13 +5,13 @@
 lead_score = 0
 
 if doc.source:
-    lead_score = lead_score + 10
+    lead_score += 10
 
 if doc.industry:
-    lead_score = lead_score + 20
+    lead_score += 20
 
 if doc.market_segment:
-    lead_score = lead_score + 30
+    lead_score += 30
 
 # Update the lead score field
 doc.lead_score = lead_score
